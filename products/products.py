5d0ab32425ae2db453d26480d4f21227c94320d2
Sebastian Kapičák <sebastian.kapicak@student.vitkovickastredni.cz>
2024-03-19 13:19:46 +0100

    CV-nový produkt v API

--- products/products.py
+++ products/products.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from API.api import GetAllProducts, GetSingleProducts, GetAllProductsCategory
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
@@ -26,3 +26,55 @@ def detailOfProduct(id):
     filtered_products = [product for product in categoryProduct if product["id"] != id]
     fourProducts = filtered_products[:l]
     return render_template('products/detail.html',length = l, id=id, detailOfProduct=data, features=fourProducts)
+
+@products_bp.route('/products/add', methods=['GET','POST'])
+def uploadProduct():
+    if request.method == 'POST':
+        # Process form data
+        product_name = request.form.get('productName')
+        product_description = request.form.get('productDescription')
+        product_price = request.form.get('productPrice')
+        product_category = request.form.get('productCategory')
+
+        # Ensure all required fields are filled
+        if not all([product_name, product_description, product_price, product_category]):
+            return "Please fill in all the form fields."
+
+        # Generate some random data for demonstration
+        random_int = random.randint(100, 1000)
+        random_rate = random.uniform(0.0, 5.0)
+        image = "static/img/noimage.png"
+
+        # Prepare product data
+        product_data = {
+            'title': product_name,
+            'price': float(product_price),
+            'description': product_description,
+            'category': product_category,
+            'image': image,
+            'rating': {'rate':random_rate,'count':random_int}
+        }
+
+        # Send data to API
+        fakestore_api_url = "https://fakestoreapi.com/products"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(fakestore_api_url, data=json.dumps(product_data), headers=headers)
+
+        # Check response and handle accordingly
+        if response.status_code == 201:
+            return "Upload successful"
+        else:
+            return "Upload failed."
+
+    # If it's a GET request, render the form
+    MyData = GetAllProducts()
+    categories = set(product["category"] for product in MyData)
+    sortedCategory = sorted(categories)
+    categories_count = {}
+    for product in MyData:
+        category = product["category"]
+        categories_count[category] = categories_count.get(category, 0) + 1
+    return render_template('products/new-product.html', products=MyData, categories=sortedCategory, pocetProduktu=categories_count)
+
+if __name__ == '__main__':
+    app.run(debug=True)
